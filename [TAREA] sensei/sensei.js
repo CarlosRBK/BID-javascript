c20e4cd7a0bdf6e43c43299da72945634ff65764
Carlos Benitez <ruli.bkrug@gmail.com>
2023-01-09 00:46:38 -0300

    Create sensei.js
    [TAREA] sensei

---
+++ [TAREA] sensei/sensei.js
@@ -0,0 +1,39 @@
+class Ninja {
+    constructor(nombre, velocidad =3, fuerza=3, salud=100) {
+        this.nombre = nombre,
+        this.salud = salud,
+        this.velocidad = velocidad,
+        this.fuerza = fuerza;
+    }
+    sayName(){
+        console.log("El nombre del ninja es: " + this.nombre)
+    }
+    showStats(){
+        console.log(this.nombre+", Fuerza: "+this.fuerza+", Velocidad: "+this.velocidad+", Salud: "+ this.salud);
+    }
+    drinkSake(){
+        return "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(nombre){
+    super(nombre, 10, 10, 200);
+    this.sabiduria = 10;
+    };
+
+    speakWisdom() {
+        const message = super.drinkSake();
+        console.log(message);
+    }
+};
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
